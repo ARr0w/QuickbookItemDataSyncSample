e6282b1ad653ae2980156594dae0fc9b795be665
Akif ul Aziz <ARr0w@users.noreply.github.com>
2025-02-23 22:44:03 +0500

    Update AccessTokenModel.cs
    Removed commented code

--- QuickbookItemDataSyncSample/Models/QbResponseModels/AccessTokenModel.cs
+++ QuickbookItemDataSyncSample/Models/QbResponseModels/AccessTokenModel.cs
@@ -17,16 +17,3 @@ namespace QuickbookItemDataSyncSample.Models.QbResponseModels
         public int RefreshTokenExpiry { get; set; }
     }
 }
-
-//To retrieve Authorize Code
-//https://appcenter.intuit.com/app/connect/oauth2?client_id=ABSXNSkdBiw6uqXa15GpRnJvZ8ERl5jzsLT4IV4PbpIWWxAQon&scope=com.intuit.quickbooks.accounting&redirect_uri=https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl&response_type=code&state=PlaygroundAuth#/OpenIdAuthorize
-
-//string clientId = "ABSXNSkdBiw6uqXa15GpRnJvZ8ERl5jzsLT4IV4PbpIWWxAQon";
-//string redirectUri = "https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl"; // Must match what's set in QuickBooks Developer Portal
-//string scope = "com.intuit.quickbooks.accounting";
-//string state = "PlaygroundAuth#/OpenIdAuthorize"; // Random string for security
-//string authUrl = $"https://appcenter.intuit.com/connect/oauth2?client_id={clientId}&response_type=code&scope={scope}&redirect_uri={redirectUri}&state={state}";
-
-//Console.WriteLine("Open this URL in your browser:");
-//Console.WriteLine(authUrl);
-//Console.ReadKey();
